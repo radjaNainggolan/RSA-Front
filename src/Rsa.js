e832fbf8c0e0984e5ade1445b502818a0215ec24
radjaNainggolan <radenorovcanin12@gmail.com>
2023-06-17 17:07:35 +0200

    almost done

--- src/Rsa.js
+++ src/Rsa.js
@@ -1,4 +1,39 @@
+import { useState, useEffect , useRef} from "react";
+import axios from "axios";
+
 const Rsa = () => {
+
+    const [input, setInput] = useState("");
+    const [output, setOutput] = useState("");
+    const textRef = useRef(null);
+    
+    const handleCopy = () => {
+        textRef.current.select();
+        document.execCommand('copy');
+        console.log('Text copied to clipboard');
+    };
+
+    const callback = (e) => {
+        e.preventDefault();
+        let input_string = {
+            content:input
+        };
+
+        axios.get("http://localhost",input_string)
+        .then(res => {
+            if(res.status !== 200){
+                alert(res.status+"\n"+res.message);
+            }
+
+            setOutput(res.data)
+
+        }
+            
+        )
+
+    }
+
+
     return (
     <div className="font-bai">
         <div className="flex flex-col justify-center align-middle text-5xl h-max gap-y-10 bg-purple-950 py-10">
@@ -12,22 +47,26 @@ const Rsa = () => {
                 <h3 className="text-2xl text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dolores labore voluptatem. </h3>
             </div>
         </div>
-        <div className="flex w-full gap-x-10 px-24 py-10">
+        <div className="flex w-full gap-x-10 justify-center align-middle  px-24 py-10 ">
             <div className="flex flex-col gap-y-7 w-5/12">
                 <div className="flex flex-col gap-y-7 w-full">
                     <label htmlFor="" className="text-xl">Input String:</label>
-                    <textarea type="textarea" className="h-64 border-2 rounded-lg  border-purple-900 focus:outline-none"/>
+                    <textarea type="textarea" onChange={ e => setInput(e.target.value)} value={input} className="resize-none h-64 border-2 rounded-lg  border-purple-900 focus:outline-none"/>
                     <div className="flex flex-row align-middle justify-center gap-x-16">
-                        <button className="rounded-xl border-2 w-24 h-8 bg-slate-700 text-white hover:scale-110 transition-all ">Generate</button>
-                        <button className="rounded-xl border-2 w-24 h-8 hover:scale-110 transition-all ">Clear</button>
+                        <button onClick={callback} className="rounded-xl border-2 w-24 h-8 bg-slate-700 text-white hover:scale-110 transition-all ">Generate</button>
+                        <button onClick={() => setInput("")} className="rounded-xl border-2 w-24 h-8 hover:scale-110 transition-all ">Clear</button>
                     </div>
                 </div>
                 <div>
-                
+                   
                 </div>
             </div>
-            <div>
-
+            <div className="flex flex-col gap-y-7 w-5/12">
+                <label className="text-xl" htmlFor="">RSA output:</label>
+                <textarea ref={textRef} className="h-64 border-2 rounded-lg  border-purple-900 focus:outline-none resize-none" readOnly value={output} name="" id="" cols="30" rows="10"></textarea>
+                <div className="flex flex-row align-middle justify-center gap-x-16">
+                    <button onClick={handleCopy} className="rounded-xl border-2 w-24 h-8 hover:scale-110 transition-all ">Copy</button>
+                </div>
             </div>
         </div>
     </div>
